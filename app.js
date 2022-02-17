d34bc92b431c35a048a33ee607ad3148b5182a2b
Evgenia-N <jazzpenguin@yandex.ru>
2022-02-17 21:21:22 +0300

    fix: mongoose installed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
@@ -7,4 +8,10 @@ const app = express();
 app.listen(PORT, () => {
     // Если всё работает, консоль покажет, какой порт приложение слушает
     console.log(`App listening on port ${PORT}`)
-}) 
+})
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+    useFindAndModify: false
+});
