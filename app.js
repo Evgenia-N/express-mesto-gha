5c55c22eadcab1358b5e8f2ec2a055c8109dc624
Evgenia-N <jazzpenguin@yandex.ru>
2022-02-19 01:33:13 +0300

    update: app.js

--- app.js
+++ app.js
@@ -8,17 +8,16 @@ const app = express();
 
 app.use(userRoutes);
 app.use(cardRoutes);
+app.use('*', (req, res)=>{
+  res.status(404).send({message: 'Страница по указанному адресу не найдена'})
+});
 
-async function main() {
-  await mongoose.connect('mongodb://localhost:27017/mestodb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  console.log(`Connected to Database`)
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+console.log(`Connected to Database`)
 
-  app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
-  })
-}
-
-main();
+app.listen(PORT, () => {
+console.log(`App listening on port ${PORT}`)
+})
