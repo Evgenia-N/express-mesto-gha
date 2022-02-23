1bc983894a459a183f4a6551601aa16a710d414c
Evgenia-N <jazzpenguin@yandex.ru>
2022-02-23 17:51:44 +0300

    feat: controllers cards.js

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,81 +1,85 @@
-const { response } = require('express');
 const Card = require('../models/card')
 
 exports.getCards = async (req, res) => {
   try {
     const cards = await Card.find({});
-    res.status(200).send(cards);
+    if (cards.length > 0) {
+      res.status(200).send(cards);
+    }
+    else {
+      res.status(200).send({message: "Отсутствуют данные для отображения"});
+    }
   }
   catch(err){
     console.log(err)
-    if (err.errors.name.name = 'ValidatorError') {
-      return res.status(400).send(err.message)
-    }
-    res.status(500).send({message: "Произошла ошибка", ...err})
+    res.status(500).send({message: "Произошла внутренняя ошибка сервера", ...err})
   }
 }
 
 exports.createCard = async (req, res) => {
   try {
-    const card = new Card(req.body)
-    res.status(201).send(await card.save());
-    // const { name, about, avatar } = req.body; // получим из объекта запроса имя и описание пользователя
-    //  User.create({ name, about, avatar });
+    console.log(req.body);
+    console.log(req.user._id);
+    const { name, link } = req.body;
+    const newCard = new Card({name, link, owner: req.user._id});
+    res.status(201).send(await newCard.save());
   }
   catch(err){
     console.log(err)
-    if (err.errors.name.name = 'ValidatorError') {
-      return res.status(400).send(err.message)
+    if (err.name = 'ValidatorError') {
+      return res.status(400).send({message: "Произошла ошибка при заполнении обязательных полей"})
     }
-    res.status(500).send({message: "Произошла ошибка", ...err})
+    res.status(500).send({message: "Произошла внутренняя ошибка сервера", ...err})
   }
 }
 
 exports.deleteCard = async (req, res) => {
   try {
-    const deletedCard = Card.findByIdAndRemove(req.params.cardId);
-    //await deletedCard.deleteOne(req.params.cardId)
-    res.status(200).send(deletedCard);
+    const deletedCard = ({_id: req.params.cardId});
+    await Card.findOneAndRemove({_id: req.params.cardId});
+    res.status(200).send({message:"Следующие данные были удалены", deletedCard});
   }
   catch(err){
     console.log(err)
-    if (err.errors.name.name = 'ValidatorError') {
-      return res.status(400).send(err.message)
+    if (err.name = 'ValidatorError') {
+      return res.status(400).send({message: "Ошибка удаления фото"})
     }
-    res.status(500).send({message: "Произошла ошибка", ...err})
+    res.status(500).send({message: "Произошла внутренняя ошибка сервера", ...err})
   }
 }
 
 exports.likeCard = async (req, res) => {
   try {
-    const likedCard = Card.findByIdAndUpdate(req.params.cardId,
-    { $addToSet: { likes: req.params.id } },
+    const likedCard = ({_id: req.params.cardId});
+    await Card.findOneAndUpdate({_id: req.params.cardId},
+    { $addToSet: { likes: req.user._id } },
     { new: true },
     );
     res.status(200).send(likedCard);
   }
   catch(err){
     console.log(err)
-    if (err.errors.name.name = 'ValidatorError') {
-      return res.status(400).send(err.message)
+    if (err.name = 'ValidatorError') {
+      return res.status(400).send({message: "Ошибка проставления отметки"})
     }
-    res.status(500).send({message: "Произошла ошибка", ...err})
+    res.status(500).send({message: "Произошла внутренняя ошибка сервера", ...err})
   }
 }
 
 exports.dislikeCard = async (req, res) => {
   try {
-    const dislikedCard = Card.findByIdAndUpdate(req.params.cardId,
-    { $pull: { likes: req.params.id } },
+    const dislikedCard = ({_id: req.params.cardId});
+    await Card.findOneAndUpdate({_id: req.params.cardId},
+    { $pull: { likes: req.user._id } },
     { new: true },
   ) ;
     res.status(200).send(dislikedCard);
   }
   catch(err){
     console.log(err)
-    if (err.errors.name.name = 'ValidatorError') {
-      return res.status(400).send(err.message)
+    if (err.name = 'ValidatorError') {
+      return res.status(400).send({message: "Ошибка проставления отметки"})
     }
-    res.status(500).send({message: "Произошла ошибка", ...err})
+    res.status(500).send({message: "Произошла внутренняя ошибка сервера", ...err})
   }
 }
